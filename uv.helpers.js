1921f6bba15cde7cc459265dc0fa3b83648897b3
Angad Tendulkar <angad.tendulkar@gmail.com>
2023-07-15 19:34:49 -0400

    update ultraviolet helpers

--- uv.helpers.js
+++ uv.helpers.js
@@ -2,7 +2,7 @@
 
 const swpath = "/ultraviolet/sw.js";
 
-async function register_uv() {
+function register_uv() {
 	if (!navigator.serviceWorker) {
 		if (location.protocol != "https:" && !location.hostname.startsWith("localhost")) {
 			throw new Error("Service workers only work over HTTPS");
@@ -11,16 +11,9 @@ async function register_uv() {
 		throw new Error("Your browser does not support service workers");
 	}
 
-	await navigator.serviceWorker.register(swpath, { scope: __uv$config.prefix });
+	return navigator.serviceWorker.register(swpath, { scope: __uv$config.prefix });
 }
 
-async function start_uv(url) {
-	try {
-		await register_uv();
-	} catch (err) {
-		alert(err.message);
-		throw err;
-	}
-
+function start_uv(url) {
 	location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 }
